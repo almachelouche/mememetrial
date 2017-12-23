382d84a7be8dbde888bee45c52071fb1513efeb2
Alma Chelouche <alma.chelouche@gmail.com>
2017-12-23 23:02:18 +0800

    Initial Commit

---
+++ Meme-me-trial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme-me-trial
+//
+//  Created by Alma Chelouche on 12/23/17.
+//  Copyright © 2017 Alma Chelouche. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
